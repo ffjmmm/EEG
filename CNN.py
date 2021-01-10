58f36c0d4e0fedd1ae96edd13209b4e3f53bb0c1
Jieming Fan <ffjmmm@berkeley.edu>
2021-01-10 16:12:59 +0800

    update

--- CNN.py
+++ CNN.py
@@ -1,2 +1,64 @@
+import os
+import shutil
 import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
 
+import utils
+
+
+EPOCH = 100
+BATCH_SIZE = 32
+INPUT_SIZE = 10
+N_CLASS = 2
+LR = 0.01
+DROPOUT_RATE = 0.0
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+writer = SummaryWriter()
+
+if os.path.exists('runs/ECG_LR=%.3f_DROP=%.2f' % (LR, DROPOUT_RATE)):
+    shutil.rmtree('runs/ECG_LR=%.3f_DROP=%.2f' % (LR, DROPOUT_RATE))
+writer = SummaryWriter('runs/ECG_LR=%.3f_DROP=%.2f' % (LR, DROPOUT_RATE))
+
+print('Loading data ...')
+dataset_train = utils.EEGBCI_Dataset(root='./', train=True, transform=transforms.ToTensor())
+dataset_test = utils.EEGBCI_Dataset(root='./', train=False, transform=transforms.ToTensor())
+# print(train_dataset.num)
+# print(test_dataset.num)
+dataloader_train = DataLoader(dataset=dataset_train, batch_size=BATCH_SIZE, shuffle=True)
+dataloader_test = DataLoader(dataset=dataset_test, batch_size=BATCH_SIZE)
+
+print('Constructing Neural Network ...')
+fc = utils.EEGBCI_FC(DROPOUT_RATE).to(device)
+optimizer = torch.optim.Adam(fc.parameters(), lr=LR)
+loss_func = torch.nn.CrossEntropyLoss()
+
+print('Start Training ...')
+for epoch in range(EPOCH):
+    fc = fc.train()
+    avg_loss = 0.
+    for step, (data, label) in enumerate(dataloader_train):
+        data, label = data.to(device), label.to(device)
+        label = label.type(torch.long).reshape(-1)
+        out = fc(data)
+        loss = loss_func(out, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        avg_loss += loss.cpu().data.numpy()
+    avg_loss /= (step + 1)
+
+    fc = fc.eval()
+    avg_accuracy = 0.0
+    for step, (data, label) in enumerate(dataloader_test):
+        data = data.to(device)
+        out = fc(data).cpu()
+        pred = torch.max(out, 1)[1].data.numpy()
+        label = label.numpy().reshape(-1)
+        accuracy = float((pred == label).astype(int).sum()) / float(len(label))
+        avg_accuracy += accuracy
+    avg_accuracy /= (step + 1)
+    print('Epoch: ', epoch, '| train loss: %.4f' % avg_loss, '| accuracy: %.2f' % avg_accuracy)
+    writer.add_scalar('loss', avg_loss, epoch)
+    writer.add_scalar('accuracy', avg_accuracy, epoch)
--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import os
 import torch
+from torch import nn
 import pandas as pd
 import numpy as np
 
 
-class EEGBCIDataset(torch.utils.data.Dataset):
+class EEGBCI_Dataset(torch.utils.data.Dataset):
     def __init__(self, root, train, transform=None):
-        super(EEGBCIDataset, self).__init__()
+        super(EEGBCI_Dataset, self).__init__()
 
         if train:
             self.data = pd.read_csv(os.path.join(root, 'train_data.csv'))
@@ -19,14 +20,44 @@ class EEGBCIDataset(torch.utils.data.Dataset):
         self.transform = transform
 
     def __getitem__(self, item):
-        data = self.data[item]
+        data = self.data[item].reshape(-1, 10)
+        label = torch.from_numpy(self.label[item])
+        if self.transform:
+            data = self.transform(data)
+        data = data.type(torch.FloatTensor)
+        label = label.type(torch.FloatTensor)
+        return data, label
+
+    def __len__(self):
+        return self.num
 
 
+class EEGBCI_FC(nn.Module):
+    def __init__(self, dropout_rate):
+        super(EEGBCI_FC, self).__init__()
 
-data = pd.read_csv('./test_data.csv')
-print(len(data))
-data = np.asarray(data, dtype=np.float)
-label = data[:, -1:]
-data = data[:, :-1]
-print(data.shape)
-print(label)
+        self.fc1 = nn.Sequential(
+            nn.Linear(10, 32),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_rate),
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(32, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_rate),
+        )
+
+        self.fc3 = nn.Sequential(
+            nn.Linear(64, 2),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = x.reshape(-1, 10)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        out = self.fc3(x)
+        return out
